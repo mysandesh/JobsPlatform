408a7a0578a93d185f1e7a9b1356e1f3afa95bba
Sandesh Acharya <mysandesh@gmail.com>
2023-08-02 22:34:48 +0545

    Welcome Page Search Wrapper edits

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -20,6 +20,11 @@ const Welcome = () => {
         <Text style={styles.userName}>Hello User</Text>
         <Text style={styles.welcomeMessage}>Find your perfect job!</Text>
       </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput style={styles.searchInput} />
+        </View>
+      </View>
     </View>
   );
 };
