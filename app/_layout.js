046d2d19c45a6bd7c5bc4045267f07006b2abe54
Sandesh Acharya <mysandesh@gmail.com>
2023-08-02 22:40:18 +0545

    DMSans font family imported

--- app/_layout.js
+++ app/_layout.js
@@ -1,6 +1,16 @@
 import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
+  const { fontsLoaded } = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
   return <Stack />;
 };
 
