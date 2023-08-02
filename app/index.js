cf1fa6729e2bc44f06dc613e5093f1f172316f9b
Sandesh Acharya <mysandesh@gmail.com>
2023-08-02 21:53:43 +0545

    User profile display fix

--- app/index.js
+++ app/index.js
@@ -23,7 +23,7 @@ const Home = () => {
             <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
           ),
           headerRight: () => (
-            <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
           ),
           headerTitle: "",
         }}
