22e99293625577309f341fd217da6700400b78b9
Sandesh Acharya <mysandesh@gmail.com>
2023-08-05 19:28:22 +0545

    Search button for Job Types added

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -45,7 +45,13 @@ const Welcome = () => {
         <FlatList
           data={jobTypes}
           renderItem={({ item }) => (
-            <TouchableOpacity style={styles.tab(activeJobType, item)}>
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push("/search/${item}");
+              }}
+            >
               <Text>{item}</Text>
             </TouchableOpacity>
           )}
