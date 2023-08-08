383c52b40acff6da163d3b9dc63c644a00cbbac4
Sandesh Acharya <mysandesh@gmail.com>
2023-08-08 21:09:18 +0545

    Template literal fix on search field

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -49,7 +49,7 @@ const Welcome = () => {
               style={styles.tab(activeJobType, item)}
               onPress={() => {
                 setActiveJobType(item);
-                router.push("/search/${item}");
+                router.push(`/search/${item}`);
               }}
             >
               <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
