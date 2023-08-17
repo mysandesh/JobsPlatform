1e30dceb20a4f300fa49fbf145f207f354ad499a
Sandesh Acharya <mysandesh@gmail.com>
2023-08-17 22:17:30 +0545

    PopularJobs hooks import added

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
 
-import styles from './popularjobs.style'
+import styles from "./popularjobs.style";
 
 const Popularjobs = () => {
   return (
     <View>
       <Text>Popularjobs</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Popularjobs
+export default Popularjobs;
