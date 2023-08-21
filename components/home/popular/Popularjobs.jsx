0a137a5f49e76930bc32592aa2167723afd86276
Sandesh Acharya <mysandesh@gmail.com>
2023-08-21 22:01:13 +0545

    PopularJobs necessary imports added

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -7,9 +7,14 @@ import {
   ActivityIndicator,
 } from "react-native";
 
+import { useRouter } from "expo-router";
+
 import styles from "./popularjobs.style";
+import { COLORS, SIZES } from "../../../constants";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
 
 const Popularjobs = () => {
+  const router = useRouter();
   return (
     <View>
       <Text>Popularjobs</Text>
