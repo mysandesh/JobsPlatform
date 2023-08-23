9847fd24d412e3292e1e87e262b7eb187eaaa573
Sandesh Acharya <mysandesh@gmail.com>
2023-08-23 22:32:44 +0545

    Activity Indicator added

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -15,6 +15,7 @@ import PopularJobCard from "../../common/cards/popular/PopularJobCard";
 
 const Popularjobs = () => {
   const router = useRouter();
+  const isLoading = false;
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -22,6 +23,13 @@ const Popularjobs = () => {
         <TouchableOpacity>
           <Text style={styles.headerBtn}>Show all</Text>
         </TouchableOpacity>
+      </View>
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" colors={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        )}
       </View>
     </View>
   );
