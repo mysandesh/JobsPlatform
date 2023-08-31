d1d09f26266a93d0e4e389fa54a3e8bdbd097e6b
Sandesh Acharya <mysandesh@gmail.com>
2023-08-31 16:45:28 +0545

    Custom hooks added

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -2,5 +2,5 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const useFetch = () => {
-  const [first, setfirst] = useState(second);
+  const [data, setData] = useState([]);
 };
