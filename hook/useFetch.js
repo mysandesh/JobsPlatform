32c8442e23229b12f6d5533098b251d3eeef6d7c
Sandesh Acharya <mysandesh@gmail.com>
2023-09-27 21:32:23 +0545

    Rapid API for Job Search added

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,8 +1,26 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { RAPID_API_KEY } from "@env";
 
-const useFetch = () => {
+const rapidApiKey = RAPID_API_KEY;
+
+const useFetch = (endpoint) => {
   const [data, setData] = useState([]);
   const [isLoading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpooint}`,
+    headers: {
+      "X-RapidAPI-Key": rapidApiKey,
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: {
+      query: "Python developer in Texas, USA",
+      page: "1",
+      page: "1",
+      num_pages: "1",
+    },
+  };
 };
