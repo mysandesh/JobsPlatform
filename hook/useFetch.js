885bce8d7d20725d49475c7cc8b0ecb673c85449
Sandesh Acharya <mysandesh@gmail.com>
2023-10-03 19:06:58 +0545

    Jobs API fetch logic added

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -18,4 +18,30 @@ const useFetch = (endpoint, query) => {
     },
     params: { ...query },
   };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert("There is an error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
 };
