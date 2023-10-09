272408524a4070b560d1665c17a17546dc8e0a01
Sandesh Acharya <mysandesh@gmail.com>
2023-10-09 21:24:23 +0545

    useFetch hook path fix

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -12,7 +12,7 @@ import { useRouter } from "expo-router";
 import styles from "./popularjobs.style";
 import { COLORS, SIZES } from "../../../constants";
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
-import useFetch from "../../../hook/fetch";
+import useFetch from "../../../hook/useFetch";
 
 const Popularjobs = () => {
   const router = useRouter();
