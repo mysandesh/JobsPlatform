195cf81bbc82d9e70a044c825699cef3dc1561a3
Sandesh Acharya <mysandesh@gmail.com>
2023-10-09 21:41:05 +0545

    PopularJobs employer logo import

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from "react-native";
 
-import styles from './popularjobcard.style'
+import styles from "./popularjobcard.style";
 
-const PopularJobCard = () => {
+const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
   return (
-    <View>
-      <Text>PopularJobCard</Text>
-    </View>
-  )
-}
+    <TouchableOpacity
+      style={styles.container(selectedJob, item)}
+      onPress={() => handleCardPress(item)}
+    >
+      <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
+        <Image source={{ uri: item.employer_logo }} />
+      </TouchableOpacity>
+    </TouchableOpacity>
+  );
+};
 
-export default PopularJobCard
+export default PopularJobCard;
