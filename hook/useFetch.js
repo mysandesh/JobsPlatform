2a2cf1ab0f166f68f4717e338d4af4dc57a531e8
Sandesh Acharya <mysandesh@gmail.com>
2023-10-09 21:29:57 +0545

    API endpoint fix

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,8 +1,5 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { RAPID_API_KEY } from "@env";
-
-const rapidApiKey = RAPID_API_KEY;
 
 const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
@@ -11,9 +8,9 @@ const useFetch = (endpoint, query) => {
 
   const options = {
     method: "GET",
-    url: `https://jsearch.p.rapidapi.com/${endpooint}`,
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": rapidApiKey,
+      "X-RapidAPI-Key": "c547b7b514msh0a0e26c7b4d6aebp1e277fjsn232cff3a97d0",
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
     params: { ...query },
