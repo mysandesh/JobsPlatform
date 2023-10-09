e7a04da479592aa27323620b4e3c7eda8ac518e6
Sandesh Acharya <mysandesh@gmail.com>
2023-10-09 21:49:18 +0545

    PopularJobs container added

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -9,7 +9,11 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
       onPress={() => handleCardPress(item)}
     >
       <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
-        <Image source={{ uri: item.employer_logo }} />
+        <Image
+          source={{ uri: item.employer_logo }}
+          resizeMode="contain"
+          style={styles.logoImage}
+        />
       </TouchableOpacity>
     </TouchableOpacity>
   );
