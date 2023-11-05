2abec39cc22a44de32df6b35c1a4b5e5fe69385e
Sandesh Acharya <mysandesh@gmail.com>
2023-11-05 19:06:17 +0545

    PopularJobs card display fix

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -36,7 +36,7 @@ const Popularjobs = () => {
           <Text>Something went wrong</Text>
         ) : (
           <FlatList
-            data={[1, 2, 3, 4]}
+            data={[...data]}
             renderItem={({ item }) => <PopularJobCard item={item} />}
             keyExtractor={(item) => item?.job_id}
             contentContainerStyle={{ columnGap: SIZES.medium }}
