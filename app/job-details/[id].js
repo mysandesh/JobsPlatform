0573229a9b48adb44c7b7015104902f7ef808379
Sandesh Acharya <mysandesh@gmail.com>
2024-01-02 17:02:12 +0545

    Essential components imported from Constants

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -9,6 +9,16 @@ import {
 
 import { Stack, useRouter, useSearchParams } from "expo-router";
 import { useCallback, useState } from "react";
+import {
+  Company,
+  JobAbout,
+  JobFooter,
+  JobTabs,
+  ScreenHeaderBtn,
+  Specifics,
+} from "../../components";
+import { COLORS, icons, SIZES } from "../../constants";
+import useFetch from "../../hook/useFetch";
 
 const JobDetails = () => {
   return <Text>JobDetails</Text>;
