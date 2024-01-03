082696287ccf3eee76a16c4fed8504b7d2f28da1
Sandesh Acharya <mysandesh@gmail.com>
2024-01-03 22:52:35 +0545

    Screen Header Button bugfix

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -37,7 +37,7 @@ const JobDetails = () => {
             <ScreenHeaderBtn
               iconUrl={icons.left}
               dimension="60%"
-              handlePress={() => router.back}
+              handlePress={() => router.back()}
             />;
           },
         }}
