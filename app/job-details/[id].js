9222a2d2f0759ee8f04754c1ae5c3aefa101323b
Sandesh Acharya <mysandesh@gmail.com>
2024-01-06 21:28:10 +0545

    Header Icons on Job Details page added

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -40,6 +40,9 @@ const JobDetails = () => {
               handlePress={() => router.back()}
             />;
           },
+          headerRight: () => {
+            <ScreenHeaderBtn iconUrl={icons.share} dimension="60%" />;
+          },
         }}
       ></Stack.Screen>
     </SafeAreaView>
