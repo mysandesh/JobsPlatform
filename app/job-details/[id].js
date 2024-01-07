e0665a19bad52f7dad7a742b5f38f3947ac6b130
Sandesh Acharya <mysandesh@gmail.com>
2024-01-07 20:35:46 +0545

    Header Title removed

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -43,6 +43,7 @@ const JobDetails = () => {
           headerRight: () => (
             <ScreenHeaderBtn iconUrl={icons.share} dimension="60%" />
           ),
+          headerTitle: "",
         }}
       ></Stack.Screen>
     </SafeAreaView>
