f54286f73ebe5b587a10dbeae9058b72b1fd03e0
Sandesh Acharya <mysandesh@gmail.com>
2024-01-15 13:23:02 +0545

    Images, Icons and Image URL Check components imported

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text, Image } from "react-native";
 
-import styles from './company.style'
+import styles from "./company.style";
+import { icons } from "../../../constants";
+import { checkImageURL } from "../../../utils";
 
 const Company = () => {
   return (
     <View>
       <Text>Company</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Company
+export default Company;
